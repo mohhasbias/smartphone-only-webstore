b1feec34c7b8fb2fb46942ec152d7c6b5607c199
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2014-02-03 22:37:57 +0700

    refine accordion style and add auto scroll to active item

--- source/static/js/app.js
+++ source/static/js/app.js
@@ -1,6 +1,7 @@
 
 // accordion custom callback
 $(document).on('click.fndtn.accordion', '[data-accordion] > dd > a', function (e){
+    // add active class
     var active_class = 'active';
     
     var target = $(e.target);
@@ -13,4 +14,7 @@ $(document).on('click.fndtn.accordion', '[data-accordion] > dd > a', function (e
         siblings.removeClass(active_class);
         target.addClass(active_class);
     }
+    
+    // scroll to active class
+    $(document).scrollTop(target.offset().top - 45*2);
 });
